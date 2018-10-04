27b20a3d2285fd8f1329f95e30e37cd9adf884d9
Daniel Huffer <danielhuffer@live.com>
2018-10-04 11:32:17 +1000

    making new branch

---
+++ client/src/components/SideNav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './SideNav.css';
+
+const SideNav = () => {
+    return (
+        <div className="sidebarContainer">
+            <div class="loading">
+            <div>
+            <div class="c1"></div>
+            <div class="c2"></div>
+            <div class="c3"></div>
+            <div class="c4"></div>
+            </div>
+            </div>
+            <ul>
+                <li>DaonzDirekt</li>
+            </ul>
+        </div>
+    );
+};
+
+export default SideNav;
